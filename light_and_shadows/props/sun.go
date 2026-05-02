20be79ea2308b57cc9b7d5dd3c7f9012d50c6130
DylanMines <dylan.e.mines@gmail.com>
2026-05-02 00:18:48 -0600

    finishing some stuff up

--- light_and_shadows/props/sun.go
+++ light_and_shadows/props/sun.go
@@ -1,6 +1,41 @@
 components {
   id: "sun"
   component: "/light_and_shadows/sun.script"
+  properties {
+    id: "color"
+    value: "0.0, 0.0, 0.0, 0.0"
+    type: PROPERTY_TYPE_VECTOR4
+  }
+  properties {
+    id: "shadow"
+    value: "1.0, 1.0, 1.0, 0.0"
+    type: PROPERTY_TYPE_VECTOR4
+  }
+  properties {
+    id: "near"
+    value: "0.1"
+    type: PROPERTY_TYPE_NUMBER
+  }
+  properties {
+    id: "far"
+    value: "1000.0"
+    type: PROPERTY_TYPE_NUMBER
+  }
+  properties {
+    id: "perspective"
+    value: "true"
+    type: PROPERTY_TYPE_BOOLEAN
+  }
+  properties {
+    id: "fov"
+    value: "2.8"
+    type: PROPERTY_TYPE_NUMBER
+  }
+  properties {
+    id: "static"
+    value: "false"
+    type: PROPERTY_TYPE_BOOLEAN
+  }
 }
 embedded_components {
   id: "label"
