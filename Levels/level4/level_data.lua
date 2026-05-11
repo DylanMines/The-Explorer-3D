6554015a02293e748e97b9f63d3c5153a46ed9d4
DylanMines <dylan.e.mines@gmail.com>
2026-05-10 20:59:11 -0600

    labrynth prompt

--- Levels/level4/level_data.lua
+++ Levels/level4/level_data.lua
@@ -1,5 +1,15 @@
 local M = {}
 
-M.name = "maze"
-M.prompts = {}
+M.name = "the labrynth"
+M.prompts = {
+    [1] = {
+        events = 
+        {
+            {text = "This level is called the labrynth"},
+            {text = "It is exceptionally larger than the other maps"},
+            {text = "be warned, you cannot save your game"},
+            {text = "good luck. If you finish, let me know what you think"}
+        }
+    }
+}
 return M
