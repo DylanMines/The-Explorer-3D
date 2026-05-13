5bc08e54b03645ed873e7a2688faaa8a38274af2
Dylan Mines <dylan.e.mines@gmail.com>
2026-05-12 22:11:15 -0600

    fixes and favicon

--- Assets/Prefabs/tile_mesh.go
+++ Assets/Prefabs/tile_mesh.go
@@ -4,7 +4,7 @@ embedded_components {
   data: "material: \"/light_and_shadows/materials/model/model_local.material\"\n"
   "vertices: \"/Assets/Meshes/quad.buffer\"\n"
   "textures: \"/Assets/Images/tilemap.png\"\n"
-  "textures: \"\"\n"
+  "textures: \"/Assets/Images/tilemap.png\"\n"
   "textures: \"/Assets/Images/empty.png\"\n"
   "position_stream: \"position\"\n"
   "normal_stream: \"normal\"\n"
